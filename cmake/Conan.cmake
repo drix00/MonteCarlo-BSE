0c98529966540b69642cc266cbca207c88082f80
Hendrix Demers <hendrix.demers@mail.mcgill.ca>
2022-12-07 00:39:20 -0500

    Add h5pp (hdf5) library

--- cmake/Conan.cmake
+++ cmake/Conan.cmake
@@ -1,42 +1,31 @@
 macro(run_conan)
-# Download automatically, you can also just copy the conan.cmake file
-if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
-  message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
-  file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/0.18.1/conan.cmake"
-          "${CMAKE_BINARY_DIR}/conan.cmake"
-          TLS_VERIFY ON)
-endif()
+    # Download automatically, you can also just copy the conan.cmake file
+    if (NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
+        message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
+        file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/0.18.1/conan.cmake"
+                "${CMAKE_BINARY_DIR}/conan.cmake"
+                TLS_VERIFY ON)
+    endif ()
 
-include(${CMAKE_BINARY_DIR}/conan.cmake)
+    include(${CMAKE_BINARY_DIR}/conan.cmake)
 
-# conan_add_remote(NAME bincrafters URL https://api.bintray.com/conan/bincrafters/public-conan)
+    set(CONAN_DISABLE_CHECK_COMPILER TRUE)
 
-set(CONAN_DISABLE_CHECK_COMPILER TRUE)
-
-#conan_cmake_configure(REQUIRES
-#        catch2/2.13.9
-#        fmt/8.1.1
-#        spdlog/1.10.0
-##        cli11/2.2.0
-#        GENERATORS cmake_find_package)
-#
-#conan_cmake_autodetect(settings)
-#
-#conan_cmake_install(PATH_OR_REFERENCE .
-#        BUILD missing
-#        REMOTE conancenter
-#        SETTINGS ${settings})
-
-conan_cmake_run(
-  REQUIRES
-  ${CONAN_EXTRA_REQUIRES}
-  catch2/2.13.9
-  spdlog/1.10.0
-  cli11/2.1.2
-  OPTIONS
-  ${CONAN_EXTRA_OPTIONS}
-  BASIC_SETUP
-  CMAKE_TARGETS # individual targets to link to
-  BUILD
-  missing)
+    conan_cmake_run(
+            REQUIRES
+            ${CONAN_EXTRA_REQUIRES}
+            catch2/2.13.9
+            fmt/8.1.1 # for spdlog
+            spdlog/1.10.0
+            cli11/2.1.2
+            eigen/3.4.0 # for h5pp
+            zlib/1.2.13 # for hdf5
+            hdf5/1.12.1 # for h5pp
+            h5pp/1.10.0
+            OPTIONS
+            ${CONAN_EXTRA_OPTIONS}
+            BASIC_SETUP
+            CMAKE_TARGETS # individual targets to link to
+            BUILD
+            missing)
 endmacro()
---
+++ src/utilities/h5pp_wrapper.h
@@ -0,0 +1,43 @@
+#ifndef MONTECARLO_BSE_H5PP_WRAPPER_H
+#define MONTECARLO_BSE_H5PP_WRAPPER_H
+
+/**
+ * @file
+ *
+ * @author Hendrix Demers <hendrix.demers@mail.mcgill.ca>
+ * @copyright 2022
+ */
+
+//   Copyright 2022 Hendrix Demers
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+// Forwards declarations
+// C system headers
+// C++ system header
+// Library headers
+#ifdef _MSC_VER
+    #pragma warning( push )
+    #pragma warning(suppress : 4244)
+    #pragma warning(suppress : 4267)
+    #pragma warning(suppress : 4702)
+    #include <h5pp/h5pp.h>
+    #pragma warning( pop )
+#else
+    #include <h5pp/h5pp.h>
+#endif
+// Project headers
+// Project private headers
+
+
+#endif// MONTECARLO_BSE_H5PP_WRAPPER_H
